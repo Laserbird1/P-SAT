fc2199785e77812a374be875f9d2be7e3296df08
Laserbird1 <edouard.debrye@gmail.com>
2020-12-26 01:32:12 +0100

    benchmarks results

--- test/instances.sh
+++ test/instances.sh
@@ -3,20 +3,31 @@
 # if [ ! which hyperfine > /dev/null ]; then {brew,apt..} install hyperfine; fi
 # if [ ! which timeout > /dev/null ]; then {brew,apt..} install coreutils; fi
 
-mkdir results 2> /dev/null
-
-nwarmup=2
+nwarmup=1
 minruns=10
-destdir=results
-
-for path in ./instances/* ;
+destdir=results_tmp
+mkdir ${destdir} 2> /dev/null
+for path in ../instances/*.txt ;
 do
     name=$(basename $path)
     jsonname=$(echo $name | cut -f1 -d'.').json
     jsonloc=${destdir}/${jsonname}
 
     if [ ! -f ${jsonloc} ]; then
-    # hyperfine 'cat {path} | timeout -s INT 10m ../bin/{ex}' \
+        hyperfine 'cat {ex}_{file} | timeout 10m ../bin/{ex} || [ $? = 124 ]' \
+        --export-json $jsonloc \
+        --min-runs $minruns \
+        --time-unit millisecond \
+        --warmup $nwarmup \
+        --cleanup 'rm -f {ex}_{file}' \
+        --prepare "if [ {ex} = dlx2 ]; then cat {path} | ../bin/converter s2k > {ex}_{file} ; else cp {path} {ex}_{file}; fi" \
+        --parameter-list ex 'dlx2','xcc-with-sparse-sets' \
+        --parameter-list path $path \
+        --parameter-list file $name ;
+    fi
+done;
+
+    # hyperfine 'cat {path} | timeout 10m ../bin/{ex} || [ $? = 124 ]' \
     # --export-json ${destdir}/s_$jsonname \
     # --min-runs $minruns \
     # --time-unit millisecond \
@@ -25,7 +36,7 @@ do
     # --parameter-list path $path \
     # --parameter-list file $name
 
-    # hyperfine 'cat {file} | timeout -s INT 10m ../bin/{ex}' \
+    # hyperfine 'cat {file} | timeout 10m ../bin/{ex} || [ $? = 124 ]' \
     # --export-json ${destdir}/k_$jsonname \
     # --min-runs $minruns \
     # --time-unit millisecond \
@@ -35,17 +46,3 @@ do
     # --parameter-list ex 'dlx2' \
     # --parameter-list path $path \
     # --parameter-list file $name 
-
-    hyperfine 'cat {ex}_{file} | timeout -s INT 10m ../bin/{ex}' \
-    --export-json $jsonloc \
-    --min-runs $minruns \
-    --time-unit millisecond \
-    --warmup $nwarmup \
-    --cleanup 'rm -f {ex}_{file}' \
-    --prepare "if [ {ex} = dlx2 ]; then cat {path} | ../bin/converter s2k > {ex}_{file} ; else cp {path} {ex}_{file}; fi" \
-    --parameter-list ex 'dlx2','xcc-with-sparse-sets' \
-    --parameter-list path $path \
-    --parameter-list file $name ;
-    fi
-done;
-
