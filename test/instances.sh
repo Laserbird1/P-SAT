43abc26877ea899d2d626ae8e824712d18dd3ed4
Laserbird1 <edouard.debrye@gmail.com>
2020-12-28 13:13:26 +0100

    refactor instances.sh

--- test/instances.sh
+++ test/instances.sh
@@ -21,29 +21,8 @@ do
         --time-unit millisecond \
         --warmup $nwarmup \
         --cleanup 'rm -f {ex}_{file}' \
-        --prepare "if [ {ex} = dlx2 ]; then cat {path} | ../bin/converter_v2 s2k > {ex}_{file} ; else cp {path} {ex}_{file}; fi" \
+        --prepare "if [ {ex} = dlx2 ]; then cat `echo ${path}` | ../bin/converter_v2 s2k > {ex}_{file} ; else cp `echo ${path}` {ex}_{file}; fi" \
         --parameter-list ex 'dlx2','xcc-with-sparse-sets' \
-        --parameter-list path $path \
         --parameter-list file $name ;
     fi
 done;
-
-    # hyperfine 'cat {path} | timeout 10m ../bin/{ex} || [ $? = 124 ]' \
-    # --export-json ${destdir}/s_$jsonname \
-    # --min-runs $minruns \
-    # --time-unit millisecond \
-    # --warmup $nwarmup \
-    # --parameter-list ex 'xcc-with-sparse-sets' \
-    # --parameter-list path $path \
-    # --parameter-list file $name
-
-    # hyperfine 'cat {file} | timeout 10m ../bin/{ex} || [ $? = 124 ]' \
-    # --export-json ${destdir}/k_$jsonname \
-    # --min-runs $minruns \
-    # --time-unit millisecond \
-    # --warmup $nwarmup \
-    # --prepare 'cat {path} | ../bin/converter s2k > {file}' \
-    # --cleanup 'rm -f {file}' \
-    # --parameter-list ex 'dlx2' \
-    # --parameter-list path $path \
-    # --parameter-list file $name 
